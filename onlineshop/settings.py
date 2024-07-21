c82fd0c0382b1b770f1001998ab278e291a9c220
ogabek0423 <ogabek0423@gmail.com>
2024-07-21 13:06:57 +0500

    news

--- onlineshop/settings.py
+++ onlineshop/settings.py
@@ -124,10 +124,12 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = ['static']
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
